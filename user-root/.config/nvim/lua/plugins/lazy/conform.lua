648a346e16ecab0f450bfa0d6b5c72f77e6d097d
Marco Pas <marco.pasopas@gmail.com>
2023-11-15 20:44:44 +0200

    feat: add custom packer formatter

--- user-root/.config/nvim/lua/plugins/lazy/conform.lua
+++ user-root/.config/nvim/lua/plugins/lazy/conform.lua
@@ -1,7 +1,17 @@
 return {
-  { "stevearc/conform.nvim", opts = {
-    formatters_by_ft = {
-      sh = { "shfmt" },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        sh = { "shfmt" },
+        hcl = { "packer_fmt", "terraform_fmt" },
+      },
+      formatters = {
+        packer_fmt = {
+          command = "packer",
+          args = { "fmt", "-" },
+        },
+      },
     },
-  } },
+  },
 }
