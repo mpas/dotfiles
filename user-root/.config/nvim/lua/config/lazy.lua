ce2b83657d4e01bd61dbaf1835abbbf10d88c0e6
Marco Pas <marco.pasopas@gmail.com>
2023-11-15 20:42:51 +0200

    feat: add copilot

--- user-root/.config/nvim/lua/config/lazy.lua
+++ user-root/.config/nvim/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
 
     -- coding
     { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.coding.copilot" },
 
     -- dap
     { import = "lazyvim.plugins.extras.dap.core" },
