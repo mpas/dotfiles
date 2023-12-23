9a84f95015ec17cc30e6905cddd0523dd62b100a
Marco Pas <marco.pasopas@gmail.com>
2023-12-23 15:27:27 +0100

    feat: add marp plugin

--- user-root/.config/nvim/lua/plugins/custom/marp.lua
+++ user-root/.config/nvim/lua/plugins/custom/marp.lua
@@ -1,3 +1,11 @@
 return {
-  { dir = "~/Development/private/marp-nvim" },
+  {
+    dir = "~/Development/private/marp-nvim",
+    -- "mpas/marp-nvim",
+    config = function()
+      require("marp").setup({
+        port = 9090,
+      })
+    end,
+  },
 }
