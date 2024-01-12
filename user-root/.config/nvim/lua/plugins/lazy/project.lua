322f7d823f93f551404ffa8f7dc5fe997d005900
Marco Pas <marco.pasopas@gmail.com>
2024-01-12 06:18:25 +0100

    enable local project using the .project.nvim file existence

--- user-root/.config/nvim/lua/plugins/lazy/project.lua
+++ user-root/.config/nvim/lua/plugins/lazy/project.lua
@@ -2,6 +2,8 @@ return {
   {
     "ahmedkhalf/project.nvim",
     opts = {
+      manual_mode = false,
+      show_hidden = true,
       patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".project.nvim" },
     },
   },
