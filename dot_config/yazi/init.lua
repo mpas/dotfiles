9235e06d0b39575dd2eb789b9499ea7598793828
Marco Pas <marco.pasopas@gmail.com>
2026-03-25 07:41:38 +0100

    Add .config/yazi/init.lua Add .config/yazi/keymap.toml Add .config/yazi/yazi.toml

---
+++ dot_config/yazi/init.lua
@@ -0,0 +1,8 @@
+Status:children_add(function(self)
+  local h = self._current.hovered
+  if h and h.link_to then
+    return " -> " .. tostring(h.link_to)
+  else
+    return ""
+  end
+end, 3300, Status.LEFT)
